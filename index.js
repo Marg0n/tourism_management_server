7b78839224eced9c1c42f68edb8f920444eb92ab
MarGoN <monondcosta@gmail.com>
2024-04-30 16:52:49 +0600

    added id based and email based GET

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //config
 require('dotenv').config()
-const app =express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -29,14 +29,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     const touristSpotCollection = client.db("touristSpotDB").collection("touristSpot");
+    const countriesCollection = client.db("touristSpotDB").collection("countries");
+
+    // Get data from touristSpot
+    app.get('/allTouristSpot', async (req, res) => {
+      const cursor = touristSpotCollection.find();
+      const results = await cursor.toArray();
+      res.send(results);
+    })
+
+     //  Get data by id
+    app.get('/allTouristSpot/:id', async (req, res) => {
+      // console.log(req.params.id);
+      const id = req.params.id;
+      const results = await touristSpotCollection.findOne({ _id: new ObjectId(id) });
+      console.log(results);
+      res.send(results);
+    });
+    
+    //  Get data by email holder
+    app.get('/myList/:email', async (req, res) => {
+      // console.log(req.params.email);
+      const mail = req.params.email;
+      const results = await touristSpotCollection.find({ email: mail }).toArray();
+      res.send(results);
+    });
+
+   
+
+    // Get data from Bangladesh    
+    app.get('/southeastAsia/bangladesh', async (req, res) => {
+      const cursor = countriesCollection.find();
+      const results = await cursor.toArray();
+      res.send(results);
+    })
 
     // Post data
-    app.post('/addTouristSpot',  async (req, res) => {
-        const newTouristSpot = req.body;
-        console.log(newTouristSpot);
-        const result = await touristSpotCollection.insertOne(newTouristSpot);
-        res.send(result);
+    app.post('/addTouristSpot', async (req, res) => {
+      const newTouristSpot = req.body;
+      console.log(newTouristSpot);
+      const result = await touristSpotCollection.insertOne(newTouristSpot);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -53,9 +88,9 @@ run().catch(console.dir);
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Server is running');
+  res.send('Server is running');
 });
 
 app.listen(port, () => {
-    console.log(`server listening on port: ${port}`);
+  console.log(`server listening on port: ${port}`);
 });
